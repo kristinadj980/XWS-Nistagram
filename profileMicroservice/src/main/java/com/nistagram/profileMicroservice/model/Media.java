52597f613c20b972d5a982438d9bd8ce17530511
Maja18 <majadragojlovic98@gmail.com>
2021-05-26 23:45:52 +0200

    adding docker file - profileMicroservice

--- profileMicroservice/src/main/java/com/nistagram/profileMicroservice/model/Media.java
+++ profileMicroservice/src/main/java/com/nistagram/profileMicroservice/model/Media.java
@@ -1,10 +1,12 @@
 package com.nistagram.profileMicroservice.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Media {
 
 	@Id
