712dfed853f99a76cf6d9bcb8a89b75b2b5a0068
Kristina Djuric <kristinadjuriic@gmail.com>
2021-06-04 18:20:44 +0200

    adding bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,15 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueRouter from 'vue-router';
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+Vue.use(BootstrapVue)
 Vue.use(VueAxios, axios)
 Vue.use(VueRouter)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
